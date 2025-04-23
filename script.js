cb8025d42b2ef54522d614b8f35259bef47c5ade
joaopaulofelixviana <joaorubyjoao@gmail.com>
2025-04-23 20:54:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarAlerta() {
+    alert("Parabéns! Seu futuro começa agora! 💪");
+  }
+  
+  function enviarFormulario(event) {
+    event.preventDefault();
+    alert("Mensagem enviada com sucesso! Retornaremos em breve.");
+  }
+  
